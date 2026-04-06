98ae39033ca5306047dfa0717e1dc9301b13f6d1
Ren <153684249+Uselesst@users.noreply.github.com>
2026-04-07 02:46:14 +0300

    update ci+req+testmain

---
+++ app/test_main.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_healthcheck():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()["message"] == "ORM работает"
+
+def test_users_empty():
+    response = client.get("/users")
+    assert response.status_code == 200
+    assert response.json() == []
